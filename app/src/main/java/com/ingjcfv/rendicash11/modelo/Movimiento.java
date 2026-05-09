873bd9b1c97732c27e6a5ea4bfdf96cb2c119f37
JulioFV <21011178@itsoeh.edu.mx>
2026-05-08 22:15:10 -0600

    Alertas

---
+++ app/src/main/java/com/ingjcfv/rendicash11/modelo/Movimiento.java
@@ -0,0 +1,95 @@
+package com.ingjcfv.rendicash11.modelo;
+
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Movimiento",
+        foreignKeys = {@ForeignKey(
+                entity = Proyecto.class,
+                parentColumns = "id",
+                childColumns = "id_proyecto"
+        ),
+                @ForeignKey(
+                        entity = Usuario.class,
+                        parentColumns = "id",
+                        childColumns = "id_usuario"
+                )
+        }
+)
+public class Movimiento {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public int tipo;
+    public double monto;
+    public int categoria;
+    public String descripcion;
+    public String fecha;
+    public int id_proyecto;
+    public int id_usuario;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public int getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getId_proyecto() {
+        return id_proyecto;
+    }
+
+    public void setId_proyecto(int id_proyecto) {
+        this.id_proyecto = id_proyecto;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+}
---
+++ app/src/main/java/com/ingjcfv/rendicash11/utils/Alerta.java
@@ -0,0 +1,96 @@
+package com.ingjcfv.rendicash11.utils;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.IpSecManager;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+
+import com.ingjcfv.rendicash11.R;
+
+public class Alerta {
+    private Dialog alerta;
+    private TextView txt_titulo;
+    private TextView txt_mensaje;
+    private TextView alerta_btn_cerrar_texto;
+    private CardView btnCerrar, btnConfirmar;
+    private LinearLayout bg_circulo;
+    private ImageView imgAlerta;
+    private ProgressBar progressBar;
+    private Context contexto;
+    public Alerta(Context contexto) {
+        this.contexto = contexto;
+        alerta = new Dialog(contexto);
+        alerta.setContentView(R.layout.alerta);
+        alerta.setCancelable(false);
+        alerta.setCanceledOnTouchOutside(false);
+        alerta.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        txt_titulo = alerta.findViewById(R.id.alerta_txt_titulo);
+        txt_mensaje = alerta.findViewById(R.id.alerta_txt_mensaje);
+        alerta_btn_cerrar_texto = alerta.findViewById(R.id.alerta_btn_cerrar_texto);
+        btnCerrar = alerta.findViewById(R.id.alerta_btn_cerrar);
+        btnConfirmar = alerta.findViewById(R.id.alerta_btn_confirmar);
+        bg_circulo = alerta.findViewById(R.id.ln_bg_circulo);
+        imgAlerta = alerta.findViewById(R.id.img_bg_circulo);
+        progressBar = alerta.findViewById(R.id.alerta_progress);
+        btnCerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alerta.dismiss();
+            }
+        });
+
+    }
+
+    public void AlertaConfirmacion(String titulo, String mensaje, Runnable onEliminar){
+        alerta_btn_cerrar_texto.setText("Cancelar");
+        alerta_btn_cerrar_texto.setTextColor(ContextCompat.getColor(contexto, R.color.black));
+        btnCerrar.setBackgroundColor(ContextCompat.getColor(contexto, R.color.white));
+        bg_circulo.setBackground(ContextCompat.getDrawable(contexto, R.drawable.bg_circulo_amarillo));
+        imgAlerta.setImageResource(R.drawable.ic_warning);
+        txt_titulo.setText(titulo);
+        txt_mensaje.setText(mensaje);
+        btnConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alerta.dismiss();
+                if(onEliminar != null)onEliminar.run();
+            }
+        });
+        alerta.show();
+    }
+    public void AlertaCarga(String titulo, String mensaje){
+        btnCerrar.setVisibility(View.GONE);
+        btnConfirmar.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+        bg_circulo.setBackground(ContextCompat.getDrawable(contexto, R.drawable.bg_circulo_azul));
+        txt_titulo.setText(titulo);
+        txt_mensaje.setText(mensaje);
+        alerta.show();
+    }
+    public void AlertaError(String titulo, String mensaje){
+        btnConfirmar.setVisibility(View.GONE);
+        bg_circulo.setBackground(ContextCompat.getDrawable(contexto, R.drawable.bg_circulo_rojo));
+        btnCerrar.setBackgroundColor(ContextCompat.getColor(contexto, R.color.rojo_oscuro));
+        txt_titulo.setText(titulo);
+        txt_mensaje.setText(mensaje);
+        imgAlerta.setImageResource(R.drawable.ic_error);
+        alerta.show();
+    }
+    public void AlertaExitosa(){
+        btnConfirmar.setVisibility(View.GONE);
+        alerta.show();
+    }
+    public void cerrarDialogo(){
+        alerta.dismiss();
+    }
+
+}
